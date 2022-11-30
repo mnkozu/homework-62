1a18b8ce94d3dff145391f6289ac683f45fce8b9
Nurkyz Mamatnazar <nurkyzmamatnazar@gmail.com>
2022-11-30 17:13:29 +0600

    Компонент About Us закончен.

--- src/containers/AboutUs/AboutUs.tsx
+++ src/containers/AboutUs/AboutUs.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import './AboutUs.css';
 
 const AboutUs = () => {
   return (
-    <div>
-      AboutUs
-    </div>
+    <div className="text-end text-white font-monospace px-3 about-us">
+        <p className="m-0">We are a shop who wants to give people beauty and warmth at the same time.</p>
+        <p className="m-0">We have invested all our love in Ozgocho.</p>
+        <p className="m-0">My team and I are always ready to help you find the thing you want.</p>
+        <p className="m-0">All our products are of high quality and at the same time at an affordable price.</p>
+      </div>
   );
 };
 
